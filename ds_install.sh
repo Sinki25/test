008a38026b18522966f4db21b9e9665673873186
Sinki25 <ms.sinkovets@mail.ru>
2020-11-20 11:42:11 +0300

    Add files via upload

---
+++ ds_install.sh
@@ -0,0 +1,7 @@
+sudo yum install unixODBC
+
+wget -O DataSunrise_Suite_6_3_1_31046.linux.64bit.run $1
+
+chmod +x DataSunrise_Suite_6_3_1_31046.linux.64bit.run
+
+./DataSunrise_Suite_6_3_1_31046.linux.64bit.run install
