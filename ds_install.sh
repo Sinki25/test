8a008e50ab15b635dab24bec37dea2b158b388ab
Sinki25 <ms.sinkovets@mail.ru>
2020-11-20 15:43:20 +0300

    Update ds_install.sh

--- ds_install.sh
+++ ds_install.sh
@@ -1,15 +1,15 @@
-sudo yum install unixODBC
+sudo yum install unixODBC -y
 
-echo "unixODBC install OK" >>> /home/test.txt 
+echo "unixODBC install OK" >> /home/test.txt 
 
 wget -O DataSunrise_Suite_6_3_1_31046.linux.64bit.run $1
 
-echo "DS download OK" >>> /home/test.txt
+echo "DS download OK" >> /home/test.txt
 
 chmod +x DataSunrise_Suite_6_3_1_31046.linux.64bit.run
 
-echo "chmod OK" >>> /home/test.txt
+echo "chmod OK" >> /home/test.txt
 
 ./DataSunrise_Suite_6_3_1_31046.linux.64bit.run install
 
-echo "DS install OK" >>> /home/test.txt
+echo "DS install OK" >> /home/test.txt
