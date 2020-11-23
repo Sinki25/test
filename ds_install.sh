038d85404824714499da30099b2fe3cb8bafa9ad
Sinki25 <ms.sinkovets@mail.ru>
2020-11-23 19:36:34 +0300

    Update ds_install.sh

--- ds_install.sh
+++ ds_install.sh
@@ -12,7 +12,9 @@ echo "chmod OK" >> /home/test.txt
 
 ./DataSunrise_Suite_6_3_1_31046.linux.64bit.run --target tmp install -f --no-password --no-start
 
-echo "DS install OK" >> /home/test.txt
+echo $? >> /home/test.txt
+
+echo "Exit code after installation" >> /home/test.txt
 
 cd /opt/datasunrise
 
@@ -29,3 +31,7 @@ FIREWALL_SERVER_BACKEND_PORT=11000 \
 FIREWALL_SERVER_CORE_PORT=11001 \
 FIREWALL_SERVER_BACKEND_HTTPS=1 \
 FIREWALL_SERVER_CORE_HTTPS=1
+
+echo $? >> /home/test.txt
+
+echo "Exit code after dictionary configuration" >> /home/test.txt
