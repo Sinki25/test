2874fd1b11373d98299b4301461487495f701873
Sinki25 <ms.sinkovets@mail.ru>
2020-11-23 15:20:45 +0300

    Update ds_install.sh

--- ds_install.sh
+++ ds_install.sh
@@ -10,6 +10,6 @@ chmod +x DataSunrise_Suite_6_3_1_31046.linux.64bit.run
 
 echo "chmod OK" >> /home/test.txt
 
-./DataSunrise_Suite_6_3_1_31046.linux.64bit.run install
+./DataSunrise_Suite_6_3_1_31046.linux.64bit.run install --remote-config -v --dictionary-type postgresql --dictionary-host $2 --dictionary-port 5432 --dictionary-database datasunrise_db --dictionary-login $3 --dictionary-password $4 --server-name katya --server-host $5 --server-port 11000
 
 echo "DS install OK" >> /home/test.txt
