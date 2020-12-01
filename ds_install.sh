fde651a13850b1584cdb77ceeafe69b8afd8f9af
Sinki25 <ms.sinkovets@mail.ru>
2020-12-01 15:40:24 +0300

    Update ds_install.sh

--- ds_install.sh
+++ ds_install.sh
@@ -42,11 +42,11 @@ echo "Datasunrise Suite was successfully started" >> /home/test.txt
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-sudo sh -c 'echo -e "[azure-cli] 
-name=Azure 
-CLI baseurl=https://packages.microsoft.com/yumrepos/azure-cli 
-enabled=1 
-gpgcheck=1 
+sudo sh -c 'echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 
 sudo yum install azure-cli -y
