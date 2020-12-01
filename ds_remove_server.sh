21bd9d75aa97a1f49e89eaef0c7589260373f996
Sinki25 <ms.sinkovets@mail.ru>
2020-12-01 14:08:33 +0300

    Update ds_remove_server.sh

--- ds_remove_server.sh
+++ ds_remove_server.sh
@@ -1,10 +1,10 @@
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-sudo sh -c 'echo -e "[azure-cli] \
-name=Azure \
-CLI baseurl=https://packages.microsoft.com/yumrepos/azure-cli \
-enabled=1 \
-gpgcheck=1 \
+sudo sh -c 'echo -e "[azure-cli] 
+name=Azure 
+CLI baseurl=https://packages.microsoft.com/yumrepos/azure-cli 
+enabled=1 
+gpgcheck=1 
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 
 sudo yum install azure-cli -y
@@ -25,43 +25,84 @@ sudo yum install java-1.8.0-openjdk -y
 
 touch /tmp/ds_servers.txt
 
-./executecommand.sh connect -host `hostname` -port 11000 -login admin -password 84218421
+cd /opt/datasunrise/cmdline
+
+./executecommand.sh connect -host `hostname` -port 11000 -login admin -password $1
 
 ./executecommand.sh showDsServers | grep 11000 > /tmp/ds_servers.txt
 
+ds_servers_count=`wc -l < /tmp/ds_servers.txt`
+
 ds_server_name_del=()
 
-for i in {0..$1}
+ds_server_name_cont=()
+
+for i in {0..$ds_servers_count}
 
 do
 
-    hostname=''
-
-    hostname=$(az vmss list-instances -g katya-group -n vmScaleSet_0 | jq '.[i].osProfile.computerName')
-
-    if [ "$hostname" != null]; then
-
-        hostname=`hostname | tr -d \"`
-
-        while IFS='' read -r DS_LINE || [[ -n "$DS_LINE" ]]; do
+    while IFS='' read -r DS_LINE || [[ -n "$DS_LINE" ]]; do
         
-            IFS=':'; ARG=($DS_LINE); unset IFS;
+        IFS=':'; ARG=($DS_LINE); unset IFS;
     
-            CK_DS_NAME=`echo ${ARG[0]} | tr -d '[:space:]'`
-            CK_DS_HOST_NAME=`echo ${ARG[1]} | tr -d '[:space:]'`
-            
-    
-            if [ "$CK_DS_HOST_NAME" == "$hostname" ]; then
-                continue
+        CK_DS_NAME=`echo ${ARG[0]} | tr -d '[:space:]'`
+        CK_DS_HOST_NAME=`echo ${ARG[1]} | tr -d '[:space:]'`
+        
+        for j in {0..$2}
+
+        do
+
+            hostname_scale=$(az vmss list-instances -g $3 -n $4 | jq ".[$j].osProfile.computerName")
+
+            hostname_scale="${hostname_scale//\"}"
+
+            if [ "$hostname_scale" == "$CK_DS_HOST_NAME" ]; then
+                
+                ds_server_name_cont+=($CK_DS_NAME);
+                    
             else 
-                name_del+=($CK_DS_NAME);
-                continue
+                if [[ " ${ds_server_name_del[@]} " =~ " ${CK_DS_NAME} " ]]; then
+                    
+                    continue
+                        
+                else
+                    ds_server_name_del+=($CK_DS_NAME);
+                    
+                fi
             fi
+            
+         done
 
-done < /tmp/ds_servers.txt
+    done < /tmp/ds_servers.txt
+    
+done
 
+if [ ${#ds_server_name_cont[@]} != ${#ds_server_name_del[@]} ]; then
 
+    for i in {1..${#ds_server_name_cont[@]}}
 
+    do 
+
+        for cont in ${ds_server_name_cont[@]}
+        do
+        
+            ds_server_name_del=("${ds_server_name_del[@]/$cont}") 
+
+        done
+    
+    done 
+
+    for del in ${ds_server_name_del[@]}
+
+    do 
+
+        ./executecommand.sh delDsServer -name $del
+    
+    done
 fi
 
-done
+
+
+
+
+
