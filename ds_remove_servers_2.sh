5863318b5ef02d9c6868a8d81c3d5bd90348b197
Sinki25 <ms.sinkovets@mail.ru>
2020-12-10 13:32:12 +0300

    Update ds_remove_servers_2.sh

--- ds_remove_servers_2.sh
+++ ds_remove_servers_2.sh
@@ -2,11 +2,13 @@ touch /tmp/ds_servers.txt
 
 cd /opt/datasunrise/cmdline
 
+sudo service datasunrise start
+
 echo "./executecommand.sh connect -host `hostname` -port 11000 -login admin -password $1" >> /home/test.txt
 
 echo "exit code after connection attempt" >> /home/test.txt
 
-./executeCommand.sh connect -host `hostname` -port 11000 -login admin -password $1
+./executecommand.sh connect -host `hostname` -port 11000 -login admin -password $1
 
 echo $? >> /home/test.txt 
 
