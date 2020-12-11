a64e605c69151a7438012bddf85f0dbe405d75d3
Sinki25 <ms.sinkovets@mail.ru>
2020-12-11 13:48:38 +0300

    Update ds_remove_servers_2.sh

--- ds_remove_servers_2.sh
+++ ds_remove_servers_2.sh
@@ -1,113 +1,121 @@
 touch /tmp/ds_servers.txt
 
-cd /opt/datasunrise/cmdline
-
 #sudo datasunrise service start
 
-sleep 30
+ds_connect() {
 
-echo "./executecommand.sh connect -host `hostname` -port 11000 -login admin -password $1" >> /home/test.txt
-
-echo "exit code after connection attempt" >> /home/test.txt
-
-./executecommand.sh connect -host `hostname` -port 11000 -login admin -password $1
-
-echo $? >> /home/test.txt 
-
-echo "exit code after showDsServers" >> /home/test.txt
-
-./executecommand.sh showDsServers | grep 11000 > /tmp/ds_servers.txt
-
-echo $? >> /home/test.txt
-
-ds_servers_count=`wc -l < /tmp/ds_servers.txt`
-
-echo "ds_servers_count=$ds_servers_count" >> /home/test.txt
-
-ds_server_name_del=()
-
-ds_server_name_cont=()
-
-vm_count=$(($2-1))
-echo "vm_count=$vm_count" >> /home/test.txt
-for i in {0..$ds_servers_count}
-do
-    while IFS='' read -r DS_LINE || [[ -n "$DS_LINE" ]]; do
-        
-        IFS=':'; ARG=($DS_LINE); unset IFS;
+    #echo "./executecommand.sh connect -host `hostname` -port 11000 -login admin -password $1" >> /home/test.txt
     
-        CK_DS_NAME=`echo ${ARG[0]} | tr -d '[:space:]'`
-        CK_DS_HOST_NAME=`echo ${ARG[1]} | tr -d '[:space:]'`
-        
-        echo "CK_DS_NAME=$CK_DS_NAME" >> /home/test.txt
-        
-        echo "CK_DS_HOST_NAME=$CK_DS_HOST_NAME" >> /home/test.txt
-        
-        for ((j=0; j<=$vm_count; j++))
+    cd /opt/datasunrise/cmdline
+    
+    sleep 30
 
+    ./executecommand.sh connect -host `hostname` -port 11000 -login admin -password $1
+    
+}
+
+ds_showservers() {
+
+    cd /opt/datasunrise/cmdline
+
+    ./executecommand.sh showDsServers | grep 11000 > /tmp/ds_servers.txt
+    
+}
+
+get_ds_servers_list() {
+
+    ds_servers_count=`wc -l < /tmp/ds_servers.txt`
+
+    echo "ds_servers_count=$ds_servers_count" >> /home/test.txt
+
+    ds_server_name_del=()
+
+    ds_server_name_cont=()
+
+    vm_count=$(($1-1))
+
+    echo "vm_count=$vm_count" >> /home/test.txt
+
+    for i in {0..$ds_servers_count}
+    do
+        while IFS='' read -r DS_LINE || [[ -n "$DS_LINE" ]]; do
+
+            IFS=':'; ARG=($DS_LINE); unset IFS;
+
+            CK_DS_NAME=`echo ${ARG[0]} | tr -d '[:space:]'`
+            CK_DS_HOST_NAME=`echo ${ARG[1]} | tr -d '[:space:]'`
+        
+            echo "CK_DS_NAME=$CK_DS_NAME" >> /home/test.txt
+
+            echo "CK_DS_HOST_NAME=$CK_DS_HOST_NAME" >> /home/test.txt
+
+            for ((j=0; j<=$vm_count; j++))
+
+            do
+
+                echo "j=$j" >> /home/test.txt
+
+                hostname_scale=$(az vmss list-instances -g $2 -n $3 | jq ".[$j].osProfile.computerName")
+
+                #echo "hostname_scale=$hostname_scale" >> /home/test.txt
+
+                hostname_scale="${hostname_scale//\"}"
+
+                echo "hostname_scale=$hostname_scale" >> /home/test.txt
+
+                if [ "$hostname_scale" == "$CK_DS_HOST_NAME" ]; then
+
+                    ds_server_name_cont+=($CK_DS_NAME);
+
+                    echo "CK_DS_NAME_cont=$CK_DS_NAME" >> /home/test.txt
+
+                else 
+                    if [[ " ${ds_server_name_del[@]} " =~ " ${CK_DS_NAME} " ]]; then
+
+                        continue
+
+                    else
+                        ds_server_name_del+=($CK_DS_NAME);
+
+                        echo "CK_DS_NAME_del=$CK_DS_NAME" >> /home/test.txt
+
+                    fi
+                fi
+
+             done
+
+        done < /tmp/ds_servers.txt
+
+    done
+    
+}
+ 
+remove_odd_servers () {
+ 
+     if [ ${#ds_server_name_cont[@]} != ${#ds_server_name_del[@]} ]; then
+
+        echo "ds_server_name_cont_count=${#ds_server_name_cont[@]}" >> /home/test.txt
+
+        echo "ds_server_name_del_count=${#ds_server_name_del[@]}" >> /home/test.txt
+
+        for cont in ${ds_server_name_cont[@]}
         do
 
-            echo "resource_group=$3" >> /home/test.txt
-            echo "resource_group=$3" >> /home/test.txt
-            echo "j=$j" >> /home/test.txt
-            
-            hostname_scale=$(az vmss list-instances -g $3 -n $4 | jq ".[$j].osProfile.computerName")
-            
-            echo "hostname_scale=`$(az vmss list-instances -g $3 -n $4 | jq ".[$j].osProfile.computerName")`" >> /home/test.txt
-            
-            echo "hostname_scale=$hostname_scale" >> /home/test.txt
+            ds_server_name_del=("${ds_server_name_del[@]/$cont}") 
 
-            hostname_scale="${hostname_scale//\"}"
-            
-            echo "hostname_scale=$hostname_scale" >> /home/test.txt
+            echo "cont=$cont" >> /home/test.txt
 
-            if [ "$hostname_scale" == "$CK_DS_HOST_NAME" ]; then
-                
-                ds_server_name_cont+=($CK_DS_NAME);
-                
-                echo "CK_DS_NAME_cont=$CK_DS_NAME" >> /home/test.txt
-                    
-            else 
-                if [[ " ${ds_server_name_del[@]} " =~ " ${CK_DS_NAME} " ]]; then
-                    
-                    continue
-                        
-                else
-                    ds_server_name_del+=($CK_DS_NAME);
-                    
-                    echo "CK_DS_NAME_del=$CK_DS_NAME" >> /home/test.txt
-                    
-                fi
-            fi
-            
-         done
+        done
 
-    done < /tmp/ds_servers.txt
+        for del in ${ds_server_name_del[@]}
+
+        do 
+
+            ./executecommand.sh delDsServer -name $del
+
+            echo "del=$del" >> /home/test.txt
+
+        done
+    fi
     
-done
-
-if [ ${#ds_server_name_cont[@]} != ${#ds_server_name_del[@]} ]; then
-
-    echo "ds_server_name_cont_count=${#ds_server_name_cont[@]}" >> /home/test.txt
-    
-    echo "ds_server_name_del_count=${#ds_server_name_del[@]}" >> /home/test.txt
-
-    for cont in ${ds_server_name_cont[@]}
-    do
-        
-        ds_server_name_del=("${ds_server_name_del[@]/$cont}") 
-        
-        echo "cont=$cont" >> /home/test.txt
-
-    done
-  
-    for del in ${ds_server_name_del[@]}
-
-    do 
-
-        ./executecommand.sh delDsServer -name $del
-        
-        echo "del=$del" >> /home/test.txt
-    
-    done
-fi
+}
