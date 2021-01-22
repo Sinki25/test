71fff852a684bb63881ed94b3204b595de93b480
Sinki25 <ms.sinkovets@mail.ru>
2021-01-22 17:51:08 +0300

    Update ds_setup.sh

--- ds_setup.sh
+++ ds_setup.sh
@@ -121,7 +121,7 @@ copyProxies() {
   
   logBeginAct "Copy proxy..."
   
-  for attempts in {1..70}
+  for attempts in {1..60}
   do
     
     instances=`$1/cmdline/executecommand.sh showInstances`
